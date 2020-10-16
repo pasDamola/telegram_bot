a481b6ef303428ffd4704d3ffb4a670f614405aa
Oyinda David <oyindadavid18@gmail.com>
2020-10-17 00:41:21 +0100

    bot now sends stickers

--- app.js
+++ app.js
@@ -1,33 +1,9 @@
 require('dotenv').config()
-
-const Telegraf = require('telegraf')
-const Markup = require('telegraf/markup')
-const Extra = require('telegraf/extra')
+const { Telegraf } = require('telegraf')
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
-
-bot.use(async (ctx, next) => {
-  const start = new Date()
-  await next()
-  const response_time = new Date() - start
-  const chat_from = `${ctx.message.chat.first_name} (id: ${ctx.message.chat.id})`
-  console.log(`Chat from ${chat_from} (Response Time: ${response_time})`)
-})
-
-bot.hears('hello', (ctx) => {
-    ctx.reply('<b>Hello</b>. <i>How are you today?</i>',
-      Extra.HTML()
-      .markup(Markup.inlineKeyboard([
-        Markup.callbackButton('Not bad', 'not bad'),
-        Markup.callbackButton('All right', 'all right')
-      ])))
-  })
-  bot.action('not bad', (ctx) => {
-    ctx.editMessageText('<i>Have a nice day 😊</i>',
-      Extra.HTML())
-  })
-  bot.action('all right', (ctx) => {
-    ctx.editMessageText('<i>May happiness be with you 🙏</i>',
-      Extra.HTML())
-  })
+bot.start((ctx) => ctx.reply('Welcome'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
 bot.launch()
